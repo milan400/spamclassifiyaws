f2affd236d901c07d7d87e5e1fca2906fd53d02b
milan400 <tripathimilan28@gmail.com>
2020-09-11 12:14:27 +0545

    changed

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def predict():
     return render_template('result.html', prediction = my_prediction)
 
 if __name__=='__main__':
-    app.run(debug=True)
+	app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
     
 
 
